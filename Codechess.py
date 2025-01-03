e4837fae2dcf7893bb77b8e5920022b63dee6948
Lưu Khánh Toàn <luutoan1912005@gmail.com>
2025-01-03 10:23:39 +0700

    Update 1/3/25

--- Codechess.py
+++ Codechess.py
@@ -1,14 +1,9 @@
-numbers = [1,2,3]
-x,y,z = numbers
-print(x,y,z)
-a = 5 + 2j
-print(a)
-b = 5.0
-print(b)
-c = (1,2,3)
-print(c)
-#c is tuple
-b = complex(b)
-print(b,type(b))
-d = {"name":"Salah","Club":"Liverpool"}
-print(d["name"],d["Club"])
+x = "  Hello, World"
+x = x.upper()
+print(x)
+x = x.lower()
+print(x)
+x = x.strip()
+print(x)
+x = x.replace('o','O')
+print(x)
