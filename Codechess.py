a2ae53b70607449044a19c6f49612023adc690a3
Lưu Khánh Toàn <luutoan1912005@gmail.com>
2025-01-05 09:01:10 +0700

    Update

--- Codechess.py
+++ Codechess.py
@@ -11,33 +11,4 @@ a = 10
 b = 4
 print(a % b)
 print(2**2)
-"""
-Operators in Python 
-- Python Arithmetic Operators
-+ : Cộng
-- : Trừ 
-* : Nhân 
-/ : Chia 
-% : Lấy phần dư
-** : Lũy thừa
-// : Chia lấy phần nguyên 
 
-- Python Assignment Operators (Toán Tử Gán)
-Tương Tự C++
-
-- Python Comparison Operators (Toán Tử So Sánh)
-Tương Tự C++
-
-- Python Logical Operators (Toán Tử Logic)
-and : Trả về True nếu cả 2 mệnh đề đều đúng
-or  : Trả về True nếu 1 trong 2 mệnh đề là đúng 
-not : Đảo ngược kết quả, trả về False nếu kết quả là đúng 
-
-- Python Identity Operators (Toán Tử nhận dạng)
-is : Trả về True nếu có đối tượng giống nhau 
-is not : Trả về True nếu không có đối tượng giống nhau 
-
-- Python Membership Operators (Toán Tử Thành Viên)
-in : Trả về True nếu phần tử tồn tại trong đối tượng
-in not : Trả về True nếu phần tử không tồn tại trong đối tượng 
-"""
